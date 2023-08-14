786f714cd46e07f28dae55d4e24b2049c96c11f1
Evgeny Rudenko <rudenkoevgeny@gmail.com>
2023-08-14 03:13:23 +0300

    Try 2 unpack

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -12,26 +12,8 @@ var ErrInvalidString = errors.New("invalid string")
 func Unpack(str string) (string, error) {
 	var result strings.Builder
 	var prev rune
-	//isEscaping := false
 
 	for _, r := range str {
-		/*
-			//todo: доработать
-			if r == '\\' && !isEscaping {
-					isEscaping = true
-					continue
-				}
-
-				if isEscaping {
-					if unicode.IsDigit(r) || r == '\\' {
-						result.WriteRune(r)
-						isEscaping = false
-						continue
-					} else {
-						return "", ErrInvalidString
-					}
-				}
-		*/
 		if unicode.IsDigit(r) {
 			if prev == 0 {
 				return "", ErrInvalidString
