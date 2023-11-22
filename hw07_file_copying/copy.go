d18f6e16e9608b6e43fa0e546021336390edf249
Evgeny Rudenko <rudenkoevgeny@gmail.com>
2023-11-22 03:43:59 +0300

    HW 07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+	"time"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -9,7 +14,80 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+const (
+	pbSlleepTime = 100 // пауза для прогрес бара
+	chunkSize    = 100
+)
+
+// для тестов убираем задержку при копировании и вывод прогресс бара
+var ItsATest = true
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fromFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer fromFile.Close()
+
+	fileInfo, err := fromFile.Stat()
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	if offset > fileInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	_, err = fromFile.Seek(offset, 0)
+	if err != nil {
+		return err
+	}
+
+	toFile, err := os.OpenFile(toPath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o666)
+	if err != nil {
+		return err
+	}
+	defer toFile.Close()
+
+	if limit > fileInfo.Size() || limit == 0 {
+		limit = fileInfo.Size()
+	}
+	var bar *pb.ProgressBar
+	buff := make([]byte, chunkSize)
+	total := int64(0)
+
+	if !ItsATest {
+		bar = pb.Full.Start64(100) // максимальное число в процентах
+		bar.SetRefreshRate(time.Duration(100) * time.Millisecond)
+		defer bar.Finish()
+	}
+
+	for {
+		n, err := fromFile.Read(buff)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		if n == 0 || (limit > 0 && total >= limit) {
+			break
+		}
+
+		if limit > 0 && total+int64(n) > limit {
+			n = int(limit - total)
+		}
+
+		if !ItsATest {
+			time.Sleep(pbSlleepTime * time.Millisecond)
+			bar.SetCurrent(int64(float64(total) / float64(limit) * 100))
+		}
+		_, err = toFile.Write(buff[:n])
+		if err != nil {
+			return err
+		}
+
+		total += int64(n)
+	}
+	if !ItsATest {
+		bar.SetCurrent(100)
+	}
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,106 @@
 package main
 
-import "testing"
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestOffsetExceeds(t *testing.T) {
+	to := "testdata/out_offset0_limit0.txt"
+	err := Copy("testdata/input.txt", to, 123456, 10)
+	require.EqualError(t, err, ErrOffsetExceedsFileSize.Error())
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	tests := []struct {
+		inputFile    string
+		expectedFile string
+		offset       int64
+		limit        int64
+	}{
+		{
+			inputFile:    "testdata/input.txt",
+			expectedFile: "testdata/out_offset0_limit0.txt",
+			offset:       0,
+			limit:        0,
+		},
+		{
+			inputFile:    "testdata/input.txt",
+			expectedFile: "testdata/out_offset0_limit10.txt",
+			offset:       0,
+			limit:        10,
+		},
+		{
+			inputFile:    "testdata/input.txt",
+			expectedFile: "testdata/out_offset0_limit1000.txt",
+			offset:       0,
+			limit:        1000,
+		},
+		{
+			inputFile:    "testdata/input.txt",
+			expectedFile: "testdata/out_offset0_limit10000.txt",
+			offset:       0,
+			limit:        10000,
+		},
+		{
+			inputFile:    "testdata/input.txt",
+			expectedFile: "testdata/out_offset100_limit1000.txt",
+			offset:       100,
+			limit:        1000,
+		},
+		{
+			inputFile:    "testdata/input.txt",
+			expectedFile: "testdata/out_offset6000_limit1000.txt",
+			offset:       6000,
+			limit:        1000,
+		},
+	}
+
+	//	HideProgress = true
+
+	for i, test := range tests {
+		t.Run("files should be equals", func(t *testing.T) {
+			to := fmt.Sprintf("out%v_%v_%v.txt", i, test.limit, test.offset)
+			defer os.Remove(to)
+
+			Copy(test.inputFile, to, test.offset, test.limit)
+			require.True(t, compare(test.expectedFile, to))
+		})
+	}
+}
+
+// сравнение файлов - считаем хэш исходного и получившегося файла
+func compare(file1, file2 string) bool {
+	hash1, err := getFileMD5(file1)
+	if err != nil {
+		return false
+	}
+
+	hash2, err := getFileMD5(file2)
+	if err != nil {
+		return false
+	}
+
+	return hash1 == hash2
+}
+
+func getFileMD5(file string) (string, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, f); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(hash.Sum(nil)), nil
 }
